f8d414d6f080f4622cb7d0eba6e71d913787f148
zmarcolesco <zmarcolesco@gmail.com>
2018-03-06 15:39:45 -0600

    User memory functions

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -3,18 +3,50 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
 
 static void syscall_handler (struct intr_frame *);
 
+//The bottom of the address space.
+int MAX_USER_VIRTUAL_ADDR = ((void*) 0x08048000);
+
+int deref_user_pointer_to_kernel(const void *virtualaddr);
+
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f UNUSED)
 {
   printf ("system call!\n");
   thread_exit ();
+}
+
+
+//See Section 1.5
+void verify_valid_ptr (const void *virtualaddr)
+{
+  //Need to check:
+  //null pointer
+  //pointer to unmapped virtual memory
+  //pointer to kernel virtual address space
+  if (virtualaddr < MAX_USER_VIRTUAL_ADDR || !is_user_vaddr(virtualaddr))
+    //terminate and free resources.
+    thread_exit();
+}
+
+//Dereference a valid user pointer
+int deref_user_pointer_to_kernel(const void *virtualaddr)
+{
+  // bytes within range are correct
+  // for strings + buffers?
+  check_valid_ptr(virtualaddr);
+  void *usrptr = pagedir_get_page(thread_current()->pagedir, virtualaddr);
+  if (!usrptr)
+      exit(ERROR);
+  return (int) usrptr;
 }
